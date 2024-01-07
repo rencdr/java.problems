ffc3f810fba59fcc0c009c51dbaef3aded2c95b2
rencdr <demirerencan@outlook.com>
2024-01-07 23:20:37 +0300

    first problem added

--- Problems.java
+++ Problems.java
@@ -1,1 +1,20 @@
 //This repo is for solutions to problems on sites Codewars, Hackerrank, Leetcode with java.
+/* 1.Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each.
+If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+-----------
+public class OppositesAttract {
+
+  public static boolean isLove(final int flower1, final int flower2) {
+    return (flower1 % 2 == 0 && flower2 % 2 == 1) || (flower1 % 2 == 1 && flower2 % 2 == 0);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(isLove(2, 3));  // true
+    System.out.println(isLove(2, 4));  // false
+    System.out.println(isLove(5, 7));  // true
+    System.out.println(isLove(6, 8));  // false
+  }
+}
+ */
