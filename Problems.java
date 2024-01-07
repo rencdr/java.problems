e3fc65e9b735e46b7304937a9569030d9851a92e
rencdr <demirerencan@outlook.com>
2024-01-07 23:42:57 +0300

    problem 3 added

--- Problems.java
+++ Problems.java
@@ -37,5 +37,30 @@ public class OppositesAttract {
         }
     }
 }
+ */
+/* 3.Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ -------------
+ import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        Map<Integer, Integer> numMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            
+      
+            if (numMap.containsKey(complement)) {
+                return new int[] { numMap.get(complement), i };
+            }
+            
+            numMap.put(nums[i], i);
+        }
+        
+        return new int[0];
+    }
+}
 
  */
