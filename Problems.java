1944c7045fe7230b0deca318a013b7be50969817
rencdr <demirerencan@outlook.com>
2024-01-13 22:41:29 +0300

    problem 8 added

--- Problems.java
+++ Problems.java
@@ -145,5 +145,16 @@ public class GCD {
         }
     }
 }
- 
+ */
+/*
+ 8.HELP! Jason can't find his textbook! It is two days before the test date, and Jason's textbooks are 
+ all out of order! Help him sort a list (ArrayList in java) full of textbooks by subject, so he can study 
+ before the test.
+
+ class sorter {
+  public static List<String> sort(List<String> textbooks) {
+      Collections.sort(textbooks, String.CASE_INSENSITIVE_ORDER);
+      return textbooks;
+  }
+}
  */
