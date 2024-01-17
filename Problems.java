ec63bf583b2d200dc9ac526cb3f0d1d914417967
rencdr <demirerencan@outlook.com>
2024-01-17 20:48:05 +0300

    problem 13 added

--- Problems.java
+++ Problems.java
@@ -243,8 +243,21 @@ public class Kata
 
 public class Bio {
     public String dnaToRna(String dna) {
-        String rna = dna.replace('T', 'U')
+        String rna = dna.replace('T', 'U');
         return rna;  
     } 
+}
+ */
+/*
+ 13.You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+Function should return true if it is possible and false if not.
+
+ public class Kata {
+  
+  public static boolean zeroFuel(double distanceToPump, double mpg, double fuelLeft) {
+      return distanceToPump <= mpg * fuelLeft;
+  }
+  
 }
  */
